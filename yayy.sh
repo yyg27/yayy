18c90e7aa84dc944cbcee4750666ff0ade26d726
yyg27 <yyg27@outlook.com>
2025-11-08 19:25:47 +0300

    added --nonconfirm  to line 6

--- yayy.sh
+++ yayy.sh
@@ -3,4 +3,4 @@
 selection=$(yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% --layout=reverse)
 [[ -z "$selection" ]] && exit
 
-xargs -r yay -S <<<"$selection"
+xargs -r yay -S --noconfirm <<<"$selection"
