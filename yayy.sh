e5b181bf519724f6e3c024a1c0216427b272abc7
yyg27 <yyg27@outlook.com>
2025-11-27 01:58:04 +0300

    fixed yay -Slq returning binary data instead of package names because of yay AUR decompression bug by fetching package list directly from AUR

--- yayy.sh
+++ yayy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+repo=$(yay -Slq --repo)
+aur=$(curl -s "https://aur.archlinux.org/packages.gz" | gunzip | tail -n +2 | cut -f1)
 
-selection=$(yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% --layout=reverse)
+selection=$(echo -e "$repo\n$aur" | sort -u  | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% --layout=reverse)
 [[ -z "$selection" ]] && exit
 
 xargs -r yay -S --noconfirm <<<"$selection"
+
